149965dae51a30d78f5a4394f042249f19d7b523
tamar96033 <tamar96033@gmail.com>
2025-07-27 01:18:39 +0300

    initial commit

---
+++ src/test/java/tests/ClalitPharmacyTest.java
@@ -0,0 +1,10 @@
+package tests;
+
+import org.testng.annotations.Test;
+public class ClalitPharmacyTest {
+
+    @Test
+    public static void te(){
+        System.out.println("asdfasdfadf");
+    }
+}
